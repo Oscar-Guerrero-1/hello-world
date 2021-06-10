ef818535d9d95bb991984af26438fb449b0d576a
your <your>
2021-06-10 10:17:39 -0400

    C:\Users\ovgkn\hello-world\hello-world

---
+++ hello-world/src/VariableDeclaration.java
@@ -0,0 +1,24 @@
+
+public class VariableDeclaration {
+	public static void main(String args[]) {
+		boolean on = false;
+		System.out.println(on);
+		
+		short s = 16;
+		System.out.println(s);
+		
+		int i = -32;
+		System.out.println(i);
+		
+		// f indicates the number is a float and not an double
+		float f = 389.348f;
+		System.out.println(f);
+		
+		// L indicates the number is a long and not an int
+		long l = 458416161665L;
+		System.out.println(l);
+		
+		char c = 'a';
+		System.out.println(c);
+	}
+}
